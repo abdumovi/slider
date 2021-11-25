09fcb275b487c9edea02150ed0e4d5665777d062
abdurahmon <abdumovi@gmail.com>
2021-11-25 13:36:19 +0500

    sliderend

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
-let elSlider = document.querySelector('.slider');
-let elItem = document.querySelectorAll('.item');
-let num = 0;
-function left(){
-  for(let i = num; i < elItem.length; i++){
-    elItem[i].style.zIndex = "1";
-    num +=1;
-    break;
+const items = document.querySelectorAll('.item');
+const prev = document.querySelector('.last');
+const next = document.querySelector('.next');
+
+let index = 0;
+prev.addEventListener('click',pervo);
+next.addEventListener('click',nexti);
+function counter(n){
+  for(let i of items){
+    i.classList.remove('active');
   }
-  if(num == elItem.length){
-    for(let j = 0; j < elItem.length; j++){
-      elItem[j].style.zIndex = "0";
-      num = 0;
-    }
-    console.log(num);
-  }
-}
-let ri = 0;
-let nums = [];
-for(let i = elItem.length; i > 0; i--){
-  nums[nums.length] = elItem[i];
+  items[n].classList.add('active');
 }
 
-function rigiht(){
-  for(let i = ri; i < nums.length; i++){
-    elItem[i].style.zIndex = "0";
-    ri +=1;
-    break;
-    
-  }
-  if(ri == nums.length){
-    for(let j = 0; j < nums.length; j++){
-      nums[j].style.zIndex = "1";
-      ri = nums.length;
-    }
-    console.log(num);
+function pervo(){
+  if(index == items.length - 1){
+    index = 0;
+    counter(index);
+  }else{
+    index++;
+    counter(index);
   }
 }
+function nexti(){
+  if(index == items.length - 1){
+    index = 0;
+    counter(index);
+  }else{
+    index++;
+    counter(index);
+  }
+}
+
